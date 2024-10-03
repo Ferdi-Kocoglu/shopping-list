d1ed587d0ebeadc25e6f3b3bd0b36b4a854c46e3
Ferdi Kocoglu <ferdi_kocoglu@outlook.com>
2024-10-03 13:57:44 +0300

    some changes in the test file

--- e2e-playwright/tests/shoppingListsTests.spec.js
+++ e2e-playwright/tests/shoppingListsTests.spec.js
@@ -1,15 +1,15 @@
 const { test, expect } = require("@playwright/test");
 
-test("Main page has expected title and link.", async ({ page }) => {
-    await page.goto("/");
-    await expect(page).toHaveTitle("Shared shopping lists");
-    await expect(page.locator("a")).toHaveText("Lists");
+test("Main page has expected title and the url.", async ({ page }) => {
+  await page.goto("/");
+  await expect(page).toHaveTitle("Shared shopping lists");
+  await expect(page.locator("a")).toHaveText("Lists");
 });
 
 const listName = `list${Math.random()}`;
 const itemName = `item${Math.random()}`;
 
-test("Can create a list.", async ({ page }) => {
+test("Create a shopping list.", async ({ page }) => {
   await page.goto("/lists");
   await page.locator("input[type=text]").type(listName);
   await page.getByRole('button', { name: 'Add' }).click();
@@ -17,14 +17,7 @@ test("Can create a list.", async ({ page }) => {
   await expect(page.locator(`a >> text='${listName}'`)).toHaveText(listName);
 });
 
-test("Can open a list page and the list is empty.", async ({ page }) => {
-  await page.goto("/lists");
-  await page.locator(`a >> text='${listName}'`).click();
-  await expect(page.locator("h1")).toHaveText(listName);
-  await expect(page.getByRole('listitem')).toHaveCount(0);
-});
-
-test("Can create an item.", async ({ page }) => {
+test("Add a new item to the shopping list.", async ({ page }) => {
   await page.goto("/lists");
   await page.locator(`a >> text='${listName}'`).click();
   await expect(page.locator("h1")).toHaveText(listName);
@@ -35,33 +28,13 @@ test("Can create an item.", async ({ page }) => {
   await expect(page.getByRole('listitem')).toContainText(itemName);
 });
 
-test("Can collect an item.", async ({ page }) => {
+test("Mark an item as collected.", async ({ page }) => {
   await page.goto("/lists");
   await page.locator(`a >> text='${listName}'`).click();
   await expect(page.locator("h1")).toHaveText(listName);
   await page.getByRole('button', {name: "Mark collected!"}).click();
   await expect(page.getByRole('listitem')).toHaveCount(1);
   await expect(page.locator('del')).toHaveText(itemName);
-});
-
-test("Items are in alphabetical order, active first", async ({ page }) => {
-  await page.goto("/lists");
-  await page.locator(`a >> text='${listName}'`).click();
-  await expect(page.locator("h1")).toHaveText(listName);
-  await expect(page.getByRole('button')).toHaveText("Add");
-  const itemsList = ['juice', 'apples', 'tomatoes', 'bananas', 'cheese', 'bread'];
-  for (const item of itemsList) {
-      await page.locator("input[type=text]").type(item);
-      await page.getByRole('button', {name: "Add"}).click();
-      await expect(page.locator(`li >> text='${item}'`)).toBeVisible();
-  };
-  // There is already one item in the list (itemName) and it's collected, 
-  // that's why we expect, that there are itemList.length + 1 in the list
-  // and itemName is in the end of the list.
-  await expect(page.locator('ul > li')).toHaveCount(itemsList.length + 1);
-  const resultItemList = itemsList.sort();
-  resultItemList.push(itemName);
-  await expect(page.locator('ul > li')).toContainText(resultItemList);
 });
 
 test("Deactivated list is not shown on the page", async ({ page }) => {
